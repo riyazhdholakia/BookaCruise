9f4ef9da08b581d90f7756360d0060e003a3912c
Riyazh D <riyazh1000@yahoo.com>
2019-03-18 10:18:42 -0400

    signup error fixed

---
+++ main_firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAmbS9EKpnJRTE5AAnKKgUFMkdDF7KOePk",
+    authDomain: "cruising-23cb7.firebaseapp.com",
+    databaseURL: "https://cruising-23cb7.firebaseio.com",
+    projectId: "cruising-23cb7",
+    storageBucket: "cruising-23cb7.appspot.com",
+    messagingSenderId: "322449955301"
+  };
+  firebase.initializeApp(config);
