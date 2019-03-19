35a8d837bd080c67b0c7e9e616b2f0ed4f3ad789
Riyazh D <riyazh1000@yahoo.com>
2019-03-19 10:29:20 -0400

    add onclick to rooms for DB

--- main_firebase.js
+++ main_firebase.js
@@ -3,11 +3,36 @@ function addNames() {
     var firstName = document.getElementById("firstName_field").value;
     var lastName = document.getElementById("lastName_field").value;
     var email = document.getElementById("email_field").value;
+    var destination = "";
+    var suite = "";
+    var date = "";
+    var confirmationNum= "";
     // Add a new document in collection "cities"
     db.collection("users").doc().set({
         firstName: firstName,
         lastName: lastName,
-        email: email  
+        email: email,
+        destination: destination,
+        suite: suite,
+        date: date,
+        confirmationNum: confirmationNum
+    })
+        .then(function () {
+            console.log("Document successfully written!");
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        });
+}
+
+var user = firebase.auth().currentUser;
+
+function addDestination() {
+    window.alert("hi");
+    var destination = document.getElementById("destination").value;
+    // Add a new document in collection "cities"
+    db.collection(user.currentUser).doc(user.currentUser.document).update({
+        destination: destination
     })
         .then(function () {
             console.log("Document successfully written!");
